599c68cb742b3e78ed761a5470b196dbed328de3
thomas-terpstra <ta.terpstra@student.han.nl>
2024-05-22 17:46:35 +0200

    cimmit

--- main.py
+++ main.py
@@ -14,18 +14,18 @@ def main():
                                        read_from_stub=True,
                                        stub_path='stubs/track_stubs.pkl')
     
-    # Assign PLayer Team
     team_assigner = TeamAssigner()
     team_assigner.assign_team_color(video_frames[0], 
                                     tracks['players'][0])
     
     for frame_num, player_track in enumerate(tracks['players']):
         for player_id, track in player_track.items():
-            team = team_assigner.get_player_team(video_frames[frame_num],
+            team = team_assigner.get_player_team(video_frames[frame_num],   
                                                  track['bbox'],
                                                  player_id)
-            tracks['players'][frame_num][player_id]['team'] = team
+            tracks['players'][frame_num][player_id]['team'] = team 
             tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+    
     
     # Draw output
     ## Draw object tracks
